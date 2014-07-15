604685730b87833170bcd2c3668ff70471420be8
Clement Lardeur <clem.lardeur@gmail.com>
2014-07-15 22:19:24 +0200

    add more tests for example

--- src/main/java/com/example/geoservice/resource/CityResource.java
+++ src/main/java/com/example/geoservice/resource/CityResource.java
@@ -24,7 +24,7 @@ public class CityResource {
     }
 
     @GET
-    @Path("/{id}")
+    @Path("/{id:[0-9]+}")
     public Response get(@PathParam("id") String id) {
         return Response.ok(repository.findById(parseLong(id))).build();
     }
--- src/test/java/com/example/geoservice/resource/CityResourceTest.java
+++ src/test/java/com/example/geoservice/resource/CityResourceTest.java
@@ -4,11 +4,14 @@ import com.example.geoservice.config.GeoserviceBinder;
 import com.example.geoservice.domain.City;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
+import org.glassfish.jersey.test.TestProperties;
 import org.junit.Test;
 
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Response;
 
+import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
+import static javax.ws.rs.core.Response.Status.NOT_FOUND;
 import static javax.ws.rs.core.Response.Status.OK;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,4 +29,16 @@ public class CityResourceTest extends JerseyTest {
         assertThat(response.readEntity(City.class)).isEqualTo(new City(1L, "Paris"));
     }
 
+    @Test
+    public void should_return_not_found_status_code() throws Exception {
+        Response response = target("/city/666").request().get();
+        assertThat(response.getStatus()).isEqualTo(NOT_FOUND.getStatusCode());
+        assertThat(response.readEntity(String.class)).isEqualTo("resource not found");
+    }
+
+    @Test
+    public void should_return_not_found_when_id_is_invalid() throws Exception {
+        assertThat(target("/city/BAD_ID").request().get().getStatus()).isEqualTo(NOT_FOUND.getStatusCode());
+        assertThat(target("/city/123_A").request().get().getStatus()).isEqualTo(NOT_FOUND.getStatusCode());
+    }
 }
